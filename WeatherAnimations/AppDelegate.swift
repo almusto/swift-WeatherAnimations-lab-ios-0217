1777379c0ec4cc6ed586f15c96a8a03adcd0a9eb
almusto <ales.musto@gmail.com>
2017-05-01 12:45:59 -0400

    some very minor refactoring

--- WeatherAnimations/AppDelegate.swift
+++ WeatherAnimations/AppDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.swift
-//  WeatherAnimations
-//
-//  Created by Ian Rahman on 10/11/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
+
 
 import UIKit
 
@@ -16,27 +10,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        ////////////////////// Presenting UI without the use of a main.storyboard
-        //
-        // Create a new window for the window property that
-        // comes standard on the AppDelegate class. The UIWindow
-        // is where all view controllers and views appear.
+
         window = UIWindow(frame: UIScreen.main.bounds)
-        //
-        // Create a new instance of WeatherViewController
         let mainViewController = WeatherViewController()
-        //
-        // Set the initial View Controller to our instance of WeatherViewController
         window?.rootViewController = mainViewController
-        //
-        // Present the window
         window?.makeKeyAndVisible()
-        //
-        //////////////////////
-        
+
+
         return true
     }
-
+    
 }
 
--- WeatherAnimations/WeatherViewController.swift
+++ WeatherAnimations/WeatherViewController.swift
@@ -9,53 +9,56 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-    
-    
+
+
     let sun = UIImageView(image: UIImage(named: "Sun"))
     let moon = UIImageView(image: UIImage(named: "Moon"))
     let cloud = UIImageView(image: UIImage(named: "Cloud"))
     let lightning = UIImageView(image: UIImage(named: "Lightning"))
-  var stormButton: UIButton!
+    var stormButton: UIButton!
 
     var viewCenterX: CGFloat = 0
     var viewCenterY: CGFloat = 0
 
-    
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      viewCenterX = self.view.center.x
-      viewCenterY = self.view.center.y
+        viewCenterX = self.view.center.x
+        viewCenterY = self.view.center.y
 
-      self.view.backgroundColor = UIColor.blue
+        self.view.backgroundColor = UIColor.blue
 
-      let imageViews = [sun, moon, cloud, lightning]
-      for imageView in imageViews {
-        view.addSubview(imageView)
-      }
-
-
-      stormButton = UIButton(type: UIButtonType.custom)
-      stormButton.frame = CGRect(x: Int(viewCenterX - 25), y: Int(viewCenterY + 200), width: 50, height: 50)
-      stormButton.setTitle("🌩", for: UIControlState.normal)
-      stormButton.backgroundColor = UIColor.white
-      stormButton.layer.borderWidth = 2.0
-      stormButton.layer.borderColor = UIColor.gray.cgColor
-      stormButton.addTarget(self, action: #selector(onTap(_:)), for: UIControlEvents.touchUpInside)
-      view.addSubview(stormButton)
+        let imageViews = [sun, moon, cloud, lightning]
+        for imageView in imageViews {
+            view.addSubview(imageView)
+        }
 
 
-      sun.center = CGPoint(x: viewCenterX + 300, y: viewCenterY + 500)
+        stormButton = UIButton(type: UIButtonType.custom)
+        stormButton.frame = CGRect(x: Int(viewCenterX - 25), y: Int(viewCenterY + 200), width: 50, height: 50)
+        stormButton.setTitle("🌩", for: UIControlState.normal)
+        stormButton.backgroundColor = UIColor.white
+        stormButton.layer.borderWidth = 2.0
+        stormButton.layer.borderColor = UIColor.gray.cgColor
+        stormButton.addTarget(self, action: #selector(onTap(_:)), for: UIControlEvents.touchUpInside)
+        view.addSubview(stormButton)
 
-      moon.center = CGPoint(x: viewCenterX + 300, y: viewCenterY + 500)
 
-      cloud.center = CGPoint(x: viewCenterX, y: viewCenterY)
-      cloud.alpha = 0
+        sun.center = CGPoint(x: viewCenterX + 300, y: viewCenterY + 500)
 
-      lightning.translatesAutoresizingMaskIntoConstraints = false
-      lightning.topAnchor.constraint(equalTo: cloud.bottomAnchor).isActive = true
-      lightning.centerXAnchor.constraint(equalTo: cloud.centerXAnchor).isActive = true
-      lightning.alpha = 0
+        moon.center = CGPoint(x: viewCenterX + 300, y: viewCenterY + 500)
+
+        cloud.frame = CGRect(x: (width/2) - 75, y: (height/3) * 2, width: 150, height: 75)
+        cloud.alpha = 0
+
+        lightning.translatesAutoresizingMaskIntoConstraints = false
+        lightning.topAnchor.constraint(equalTo: cloud.bottomAnchor).isActive = true
+        lightning.centerXAnchor.constraint(equalTo: cloud.centerXAnchor).isActive = true
+        lightning.alpha = 0
 
 
 
@@ -63,128 +66,128 @@ class WeatherViewController: UIViewController {
 
     }
 
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    changeBackgroundColor()
-    moveSunAndMoon()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        changeBackgroundColor()
+        moveSunAndMoon()
 
-  }
-
-
-  func changeBackgroundColor() {
-    UIView.animateKeyframes(withDuration: 20, delay: 0, options: [.allowUserInteraction, .repeat], animations: {
-      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
-        self.view.backgroundColor = UIColor.cyan
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
-        self.view.backgroundColor = UIColor.blue
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
-        self.view.backgroundColor = UIColor.black
-      }
-      UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
-        self.view.backgroundColor = UIColor.blue
-      }
-
-    }, completion: nil)
-  }
+    }
 
 
+    func changeBackgroundColor() {
+        UIView.animateKeyframes(withDuration: 20, delay: 0, options: [.allowUserInteraction, .repeat], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
+                self.view.backgroundColor = UIColor.cyan
+            }
 
-  func moveSunAndMoon() {
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+                self.view.backgroundColor = UIColor.blue
+            }
 
-    UIView.animateKeyframes(withDuration: 20, delay: 0, options: [.calculationModeCubic, .repeat], animations: {
-      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
-        self.sun.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 250)
-        self.view.layoutIfNeeded()
-      }
+            UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
+                self.view.backgroundColor = UIColor.black
+            }
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                self.view.backgroundColor = UIColor.blue
+            }
 
-      UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
-        self.sun.center = CGPoint(x: self.viewCenterX - 300, y: self.viewCenterY + 500)
-        self.view.layoutIfNeeded()
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
-        self.moon.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 250)
-        self.view.layoutIfNeeded()
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
-        self.moon.center = CGPoint(x: self.viewCenterX - 300, y: self.viewCenterY + 500)
-        self.view.layoutIfNeeded()
-      }
-
-    }, completion: {_ in
-      self.sun.center = CGPoint(x: self.viewCenterX + 300, y: self.viewCenterY + 500)
-      self.moon.center = CGPoint(x: self.viewCenterX + 300, y: self.viewCenterY + 500)
-    })
-  }
-
-  func onTap(_ sender: UIButton) {
-    moveCloud()
-    flash()
-
-  }
-
-  func moveCloud() {
-
-    UIView.animateKeyframes(withDuration: 8, delay: 0, options: [], animations: {
-      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
-        self.cloud.alpha = 1
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
-        self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 200)
-        self.view.layoutIfNeeded()
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
-        self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY)
-        self.view.layoutIfNeeded()
-      }
-
-      UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
-        self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 200)
-        self.cloud.alpha = 0
-        self.view.layoutIfNeeded()
-
-      }
-    }, completion: {_ in
-      self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY)
-
-    })
-  }
-
-  func flash() {
-    UIView.animateKeyframes(withDuration: 1, delay: 3, options: [.autoreverse], animations: {
-      UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/6) {
-        self.lightning.alpha = 1
-      }
-      UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 1/6) {
-        self.lightning.alpha = 0
-
-      }
-      UIView.addKeyframe(withRelativeStartTime: 2/6, relativeDuration: 1/6) {
-        self.lightning.alpha = 1
-
-      }
-      UIView.addKeyframe(withRelativeStartTime: 3/6, relativeDuration: 1/6) {
-        self.lightning.alpha = 0
-
-      }
-      UIView.addKeyframe(withRelativeStartTime: 4/6, relativeDuration: 1/6) {
-        self.lightning.alpha = 1
-
-      }
-      UIView.addKeyframe(withRelativeStartTime: 5/6, relativeDuration: 1/6) {
-        self.lightning.alpha = 0
-      }
-    })
-  }
+        }, completion: nil)
+    }
 
 
+
+    func moveSunAndMoon() {
+
+        UIView.animateKeyframes(withDuration: 20, delay: 0, options: [.calculationModeCubic, .repeat], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
+                self.sun.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 250)
+                self.view.layoutIfNeeded()
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+                self.sun.center = CGPoint(x: self.viewCenterX - 300, y: self.viewCenterY + 500)
+                self.view.layoutIfNeeded()
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
+                self.moon.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 250)
+                self.view.layoutIfNeeded()
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                self.moon.center = CGPoint(x: self.viewCenterX - 300, y: self.viewCenterY + 500)
+                self.view.layoutIfNeeded()
+            }
+
+        }, completion: {_ in
+            self.sun.center = CGPoint(x: self.viewCenterX + 300, y: self.viewCenterY + 500)
+            self.moon.center = CGPoint(x: self.viewCenterX + 300, y: self.viewCenterY + 500)
+        })
+    }
+
+    func onTap(_ sender: UIButton) {
+        moveCloud()
+        flash()
+
+    }
+
+    func moveCloud() {
+
+        UIView.animateKeyframes(withDuration: 8, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25) {
+                self.cloud.alpha = 1
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+                self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 200)
+                self.view.layoutIfNeeded()
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.50, relativeDuration: 0.25) {
+                self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY)
+                self.view.layoutIfNeeded()
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25) {
+                self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY - 200)
+                self.cloud.alpha = 0
+                self.view.layoutIfNeeded()
+
+            }
+        }, completion: {_ in
+            self.cloud.center = CGPoint(x: self.viewCenterX, y: self.viewCenterY)
+
+        })
+    }
+
+    func flash() {
+        UIView.animateKeyframes(withDuration: 1, delay: 3, options: [.autoreverse], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/6) {
+                self.lightning.alpha = 1
+            }
+            UIView.addKeyframe(withRelativeStartTime: 1/6, relativeDuration: 1/6) {
+                self.lightning.alpha = 0
+                
+            }
+            UIView.addKeyframe(withRelativeStartTime: 2/6, relativeDuration: 1/6) {
+                self.lightning.alpha = 1
+                
+            }
+            UIView.addKeyframe(withRelativeStartTime: 3/6, relativeDuration: 1/6) {
+                self.lightning.alpha = 0
+                
+            }
+            UIView.addKeyframe(withRelativeStartTime: 4/6, relativeDuration: 1/6) {
+                self.lightning.alpha = 1
+                
+            }
+            UIView.addKeyframe(withRelativeStartTime: 5/6, relativeDuration: 1/6) {
+                self.lightning.alpha = 0
+            }
+        })
+    }
+    
+    
 }
 
 
